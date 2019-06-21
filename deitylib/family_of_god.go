3d0eaf3f9fca7f4e8feae2179006b79a2da681b7
Jordan Gregory <jordan@gregory-dev.io>
2019-06-21 15:16:14 -0500

    add family of god

--- deitylib/family_of_god.go
+++ deitylib/family_of_god.go
@@ -1,1 +1,22 @@
 package deitylib
+
+import (
+	"github.com/google/uuid"
+)
+
+func createFamilyOfGodDeities() {
+	familyOfGodDeities := map[string]map[string]interface{}{
+		"God": {
+			"Religion": "Family of God",
+			"Sex":      "Male",
+			"Oversight": []string{
+				"Everything",
+			},
+		},
+	}
+
+	for k, v := range familyOfGodDeities {
+		id := uuid.New().String()
+		DS = DS.NewDiety(id, k, v["Religion"].(string), v["Sex"].(string), v["Oversight"].([]string))
+	}
+}
